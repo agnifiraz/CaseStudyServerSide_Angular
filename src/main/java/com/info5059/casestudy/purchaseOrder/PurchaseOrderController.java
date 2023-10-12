151e2cd034e89408ac1d5e640b6f15b6f55235aa
Agnita Paul <agnifiraz@gmail.com>
2023-10-11 23:14:44 -0400

    Updated PurchaseOrder and Item info

--- src/main/java/com/info5059/casestudy/purchaseOrder/PurchaseOrderController.java
+++ src/main/java/com/info5059/casestudy/purchaseOrder/PurchaseOrderController.java
@@ -11,8 +11,24 @@ public class PurchaseOrderController {
     @Autowired
     private PurchaseOrderDAO purchaseOrderDAO;
 
+    @Autowired
+    private PurchaseOrderRepository purchaseOrderRepository;
+
     @PostMapping("/api/purchaseorders")
     public ResponseEntity<PurchaseOrder> addOne(@RequestBody PurchaseOrder clientrep) { // use RequestBody here
         return new ResponseEntity<PurchaseOrder>(purchaseOrderDAO.create(clientrep), HttpStatus.OK);
     }
+
+    @GetMapping("/api/purchaseorders/{vendorid}")
+    public ResponseEntity<Iterable<PurchaseOrder>> findByVendor(@PathVariable Long vendorid) {
+        Iterable<PurchaseOrder> purchaseOrders = purchaseOrderRepository.findByVendorid(vendorid);
+        return new ResponseEntity<Iterable<PurchaseOrder>>(purchaseOrders, HttpStatus.OK);
+    }
+
+    @GetMapping("/api/purchaseorders") //added
+    public ResponseEntity<Iterable<PurchaseOrder>> findAll() {
+        Iterable<PurchaseOrder> pos = purchaseOrderRepository.findAll();
+        return new ResponseEntity<Iterable<PurchaseOrder>>(pos, HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/info5059/casestudy/purchaseOrder/PurchaseOrderRepository.java
+++ src/main/java/com/info5059/casestudy/purchaseOrder/PurchaseOrderRepository.java
@@ -1,5 +1,7 @@
 package com.info5059.casestudy.purchaseOrder;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -8,12 +10,14 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin
-@RepositoryRestResource(collectionResourceRel = "purchaseOrders", path = "purchaseOrders")
+@RepositoryRestResource(collectionResourceRel = "purchaseorders", path = "purchaseorders")
 public interface PurchaseOrderRepository extends CrudRepository<PurchaseOrder, Long> {
     // extend so we can return the number of rows deleted
     @Modifying
     @Transactional
     @Query("delete from PurchaseOrder where id = ?1")
     int deleteOne(Long purchaseOrderid);
+
+    List<PurchaseOrder> findByVendorid(long vendorid);
 
 }
